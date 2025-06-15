74a3b6dc30f6e1fac51137a43e2c12adbe39b16c
Bhanuka Uyanage <50510335+BhanukaC@users.noreply.github.com>
2025-06-15 08:31:41 +0530

    fixed filtering bugs

--- app/issues/IssueStatusFilter.tsx
+++ app/issues/IssueStatusFilter.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { Select } from "@radix-ui/themes";
 import React from "react";
-import { Status } from "../generated/prisma";
-import { useRouter } from "next/navigation";
 
-const statuses: { value?: Status; label: string }[] = [
-  { label: "All" },
+import { Status } from "../generated/prisma";
+import { useRouter, useSearchParams } from "next/navigation";
+
+const statuses: { value?: Status | "none"; label: string }[] = [
+  { label: "All", value: "none" },
   { value: "OPEN", label: "Open" },
   { value: "IN_PROGRESS", label: "In Progress" },
   { value: "CLOSED", label: "Closed" },
@@ -13,12 +14,25 @@ const statuses: { value?: Status; label: string }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   return (
     <Select.Root
       onValueChange={(status) => {
-        const query = status == "none" ? "" : `?status=${status}`;
+        const params = new URLSearchParams();
+        if (status !== "none" && status) {
+          params.append("status", status);
+        }
+        if (searchParams.get("orderBy")) {
+          params.append("orderBy", searchParams.get("orderBy")!);
+        }
+        const query = params.size ? `?${params.toString()}` : "";
         router.push(`/issues${query}`);
       }}
+      value={
+        searchParams && searchParams.get("status")
+          ? searchParams.get("status")!
+          : "none"
+      }
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
